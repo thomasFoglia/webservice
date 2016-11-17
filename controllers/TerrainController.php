152193f35b22504206411f12ea445c53025400ba
Thomas Foglia <thomas.foglia38@gmail.com>
2016-11-17 15:25:22 +0100

    com

---
+++ controllers/TerrainController.php
@@ -0,0 +1,46 @@
+<?php
+
+class TerrainController extends MyController
+{
+    // get one
+    // or get all
+    public function getAction($request) {
+        if(isset($request->url_elements[2])) {
+            // get id = $id_terrain
+            $id_terrain = (int)$request->url_elements[2];
+            // get in BDD
+            $data["message"] = "";
+        } else {
+            // get all
+            $data["message"] = "";
+        }
+        return $data;
+    }
+    
+    // delete
+    public function deleteAction($request) {
+        if(isset($request->url_elements[2])) {
+            $id_terrain = (int)$request->url_elements[2];
+            // delete id = $id_terrain
+            
+        }
+    }
+    
+    // update
+     public function putAction($request) {
+        $data = $request->parameters;
+        // parse parameters
+        
+        $data['message'] = "Les données ont été mise à jour";
+        return $data;
+    }
+    
+    // create
+    public function postAction($request) {
+        $data = $request->parameters;
+        // parse parameters
+        
+        $data['message'] = "Les données ont été enregistrées";
+        return $data;
+    }
+}
---
+++ library/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+class Request {
+    public $url_elements;
+    public $verb;
+    public $parameters;
+
+    public function __construct() {
+        $this->verb = $_SERVER['REQUEST_METHOD'];
+        $this->url_elements = explode('/', $_SERVER['PATH_INFO']);
+
+        $this->parseIncomingParams();
+        $this->format = 'json';
+        if(isset($this->parameters['format'])) {
+            $this->format = $this->parameters['format'];
+        }
+        return true;
+    }
+
+    public function parseIncomingParams() {
+        $parameters = array();
+
+        // get
+        if (isset($_SERVER['QUERY_STRING'])) {
+            parse_str($_SERVER['QUERY_STRING'], $parameters);
+        }
+
+        // put / post
+        $body = file_get_contents("php://input");
+        
+        $body_params = json_decode($body);
+        if($body_params) {
+            foreach($body_params as $param_name => $param_value) {
+                $parameters[$param_name] = $param_value;
+            }
+        }
+        $this->format = "json";
+        break;
+           
+        $this->parameters = $parameters;
+    }
+}
