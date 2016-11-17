8bba00048775122b0b1e6f139431ca3355f2dedb
FloSIL06 <floriandc38380@gmail.com>
2016-11-17 16:55:11 +0100

    resa

--- controllers/ReservationController.php
+++ controllers/ReservationController.php
@@ -5,25 +5,36 @@ class ReservationController
     // get one
     // or get all
     public function getAction($request) {
-        if(isset($request->url_elements[2])) {
+        $pdo = new bdd();
+        if(isset($request->url_elements[2]) && $request->url_elements[2] != '') {
+            
+
             // get id = $id_terrain
-            $id_terrain = (int)$request->url_elements[2];
+            $id_reservation = (int)$request->url_elements[2];
             // get in BDD
+            $datas =  $pdo->select('SELECT * FROM reservation WHERE id = ' . $id_reservation);
             $data["message"] = "";
         } else {
             // get all
+            $datas =  $pdo->select('SELECT * FROM reservation'); 
             $data["message"] = "";
         }
-        return $data;
+        return $datas;
     }
     
     // delete
     public function deleteAction($request) {
         if(isset($request->url_elements[2])) {
-            $id_terrain = (int)$request->url_elements[2];
+            $id_reservation = (int)$request->url_elements[2];
             // delete id = $id_terrain
-
-            $data["message"] = "Données supprimées";
+            $pdo = new bdd();
+            $nb = $pdo->exec('DELETE FROM reservation WHERE id = ' . $id_reservation);
+            
+            if ($nb == 1){
+                $data["message"] = "La reservation ". $id_reservation .' a été supprimée';
+            } else {
+                $data["message"] = "Cette réservation n'existe pas";
+            }
         }
     }
     
