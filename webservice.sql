af3a4828c249dd00888090f42772878572ae9c1e
Thomas Foglia <thomas.foglia38@gmail.com>
2016-11-25 14:18:20 +0100

    ok

--- webservice.sql
+++ webservice.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 17 Novembre 2016 à 14:16
+-- Généré le :  Ven 25 Novembre 2016 à 13:03
 -- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Version de PHP :  7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `ws_database`
+-- Base de données :  `u627327885_sq`
 --
 
 -- --------------------------------------------------------
@@ -29,8 +29,34 @@ SET time_zone = "+00:00";
 CREATE TABLE `reservation` (
   `id` int(11) NOT NULL,
   `id_terrain` int(11) NOT NULL,
-  `heure_reservation` date NOT NULL
+  `heure_reservation` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `terrain`
+--
+
+CREATE TABLE `terrain` (
+  `id` int(11) NOT NULL,
+  `available` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `terrain`
+--
+
+INSERT INTO `terrain` (`id`, `available`) VALUES
+(7, 9),
+(2, 5),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 1),
+(8, 5),
+(9, 5),
+(10, 5);
 
 --
 -- Index pour les tables exportées
@@ -43,6 +69,12 @@ ALTER TABLE `reservation`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `terrain`
+--
+ALTER TABLE `terrain`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -51,6 +83,11 @@ ALTER TABLE `reservation`
 --
 ALTER TABLE `reservation`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `terrain`
+--
+ALTER TABLE `terrain`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
